e1d9ce001c9cdb5ef0d1311f7b2a4b2061aab31f
Gabe <36393392+sfgabe@users.noreply.github.com>
2018-12-02 05:41:12 -0500

    Update fishfeeder.py
    cleaned code

--- fishfeeder.py
+++ fishfeeder.py
@@ -1,22 +1,22 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 14
+servoPIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(0.5) # Initialization
-try:
-  while True:
-    p.ChangeDutyCycle(0.5) #home
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5) #180
-    time.sleep(0.5)
-    p.ChangeDutyCycle(0.5) #home
-    time.sleep(0.5)
-    p.stop()
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
 
+p.ChangeDutyCycle(0.5) #home
+time.sleep(0.5)
+p.ChangeDutyCycle(12.5) #180
+time.sleep(0.5)
+p.ChangeDutyCycle(0.5) #home
+time.sleep(0.5)
+p.stop()
+
+GPIO.cleanup()
+exit()
