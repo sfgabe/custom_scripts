b740f104984fc29c1d03860f76db5b1224300636
Gabe <36393392+sfgabe@users.noreply.github.com>
2019-01-10 01:17:39 -0500

    Update fishfeeder.py
    switched polarity for better scooping

--- fishfeeder.py
+++ fishfeeder.py
@@ -7,14 +7,14 @@ servoPIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
 p.start(0.5) # Initialization
 
-p.ChangeDutyCycle(0.5) #home
+p.ChangeDutyCycle(12.5) #home
 time.sleep(0.5)
-p.ChangeDutyCycle(12.5) #180
+p.ChangeDutyCycle(0.5) #180
 time.sleep(0.5)
-p.ChangeDutyCycle(0.5) #home
+p.ChangeDutyCycle(12.5) #home
 time.sleep(0.5)
 p.stop()
 
