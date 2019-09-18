7168496b55e68d7a562ba199bb6ea85981296fcd
Gabe <36393392+sfgabe@users.noreply.github.com>
2019-09-18 02:03:41 -0400

    Added watermark layer
    stolen from here https://github.com/yearnfar/watermark/blob/master/main.go

--- camera.go
+++ camera.go
@@ -7,7 +7,17 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"io/ioutil"
+	"log"
+	"os"
+	"unicode/utf8"
 
+	"github.com/disintegration/imaging"
+	"github.com/golang/freetype"
 	"github.com/reef-pi/reef-pi/controller"
 	"github.com/reef-pi/reef-pi/controller/storage"
 	"github.com/reef-pi/reef-pi/controller/utils"
@@ -136,5 +146,69 @@ func (c *Controller) uploadImage(imgName string) {
 	err := utils.Command(parts[0], parts[1:]...).WithDevMode(c.DevMode).Run()
 	if err != nil {
 		log.Println("ERROR: Failed to upload image. Command:", command, "Error:", err)
+	}
+}
+
+func (c *Controller) watermark() {
+	srcImg, err := imaging.Open(imgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//workpath := time.Now().Unix()
+
+	text := fmt.Sprintf(imgPath, time.Now().Format("15-04-05-Mon-Jan-2-2006"))
+	textImg := makeTextImage(text, 32, 60)
+
+	imaging.Save(textImg, "text_img.png")
+
+	offset := image.Pt(srcImg.Bounds().Dx()-textImg.Bounds().Dx(), srcImg.Bounds().Dy()-textImg.Bounds().Dy())
+
+	newImg := image.NewRGBA(srcImg.Bounds())
+	draw.Draw(newImg, srcImg.Bounds(), srcImg, image.ZP, draw.Src)
+	draw.Draw(newImg, textImg.Bounds().Add(offset), textImg, image.ZP, draw.Over)
+
+	output, err := os.Create("watermark.jpg")
+	//output, err := os.Create(fmt.Sprintf("watermark_%d.jpg", timestamp))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer output.Close()
+	err = jpeg.Encode(output, newImg, &jpeg.Options{Quality: 100})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func (c *Controller) makeTextImage(text string, fontsize, rotate float64) *image.NRGBA {
+	fontBytes, err := ioutil.ReadFile("msyh.ttc")
+	if err != nil {
+		log.Println(err)
+	}
+
+	font, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+	}
+
+	ctx := freetype.NewContext()
+	ctx.SetDPI(72)
+	ctx.SetFont(font)
+	ctx.SetFontSize(fontsize)
+	ctx.SetSrc(image.NewUniform(color.RGBA{R: 0, G: 0, B: 0, A: 255}))
+
+	txtImg := image.NewNRGBA(image.Rect(0, 0, int(fontsize)*utf8.RuneCountInString(text), int(fontsize)))
+	ctx.SetClip(txtImg.Bounds())
+	ctx.SetDst(txtImg)
+
+	pt := freetype.Pt(0, int(-fontsize/6)+ctx.PointToFixed(fontsize).Ceil())
+	ctx.DrawString(text, pt)
+
+	if rotate > 0 {
+		return imaging.Rotate(txtImg, rotate, color.Transparent)
+	} else {
+		return txtImg
 	}
 }
