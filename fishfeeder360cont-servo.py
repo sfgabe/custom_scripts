65320da6d02166568289944906d136f07d2866c9
Gabe <36393392+sfgabe@users.noreply.github.com>
2019-11-25 22:53:45 -0500

    Update fishfeeder360cont-servo.py

--- fishfeeder360cont-servo.py
+++ fishfeeder360cont-servo.py
@@ -2,31 +2,20 @@
 ## continuous rotation servo for an auger style fish feeder
 ## https://www.thingiverse.com/thing:301532
 
+import RPi.GPIO as GPIO
 import time
-import wiringpi
 
 servoPIN = 12
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(servoPIN, wiringpi.GPIO.PWM_OUTPUT)
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
-
-delay_period = 0.02
-backup_period = 0.001
+p = GPIO.PWM(servoPIN, 50) # GPIO 12 for PWM with 50Hz
+p.start(1.5) # Initialization
 
 def dispensefood():
-    wiringpi.pwmWrite(servoPIN,0)
     print("Feeding the Fish")
-    for pulse in range(50, 250, 1):
-           wiringpi.pwmWrite(servoPIN, pulse)
-           time.sleep(delay_period)
-    print("Backing up...")
-    for pulse in range(250, 50, -1):
-            wiringpi.pwmWrite(servoPIN, pulse)
-            time.sleep(backup_period)
-    wiringpi.pwmWrite(servoPIN,0)
+    p.ChangeDutyCycle(2)
+    time.sleep(0.2)
+    p.stop()
 
 dispensefood()
