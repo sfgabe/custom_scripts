23860e2564a610e69b4afe0bafd387d8b3310c59
Gabe <36393392+sfgabe@users.noreply.github.com>
2020-01-08 03:17:50 -0500

    Update octoprint_relay_toggle.sh

--- octoprint_relay_toggle.sh
+++ octoprint_relay_toggle.sh
@@ -1,10 +1,33 @@
-#Toggles 2 Relays at once, for use in OctoPrint Enclosure
-#!/bin/sh
+#!/bin/bash
 
-Relay1On="http://octopi/plugin/enclosure/setIO?status=true&index_id=2&apikey=YOURAPIKEY"
-Relay1Off="http://octopi/plugin/enclosure/setIO?status=false&index_id=2&apikey=YOURAPIKEY"
-Relay2On="http://octopi/plugin/enclosure/setIO?status=true&index_id=3&apikey=YOURAPIKEY"
-Relay2Off="http://octopi/plugin/enclosure/setIO?status=false&index_id=3&apikey=YOURAPIKEY"
+## Toggles 2 Relays at once, for use in OctoPrint Enclosure
+##
+## NOTE: Octoprint enclosure plugin has some permissions issues. 
+## This needs to be set up as a service in order to reliably run
+## /lib/systemd/system/octoprint_power_toggle.service
+## ...then run as a shell script in the enclosure plugin with "sudo systemctl start octoprint_power_toggle"
+## ----
+## [Unit]
+## Description=Run Printer Power Toggle
+## After=octoprint.service
+##
+## [Service]
+## ExecStart=/bin/bash /home/pi/scripts/printer_relays
+## Restart=never
+## StandardOutput=syslog
+## StandardError=syslog
+## SyslogIdentifier=Power-Toggle
+## User=root
+## Group=root
+##
+## [Install]
+## WantedBy=multi-user.target
+##
+
+Relay1On="http://octopi/plugin/enclosure/setIO?status=true&index_id=1&apikey=YOURAPIKEY"
+Relay1Off="http://octopi/plugin/enclosure/setIO?status=false&index_id=1&apikey=YOURAPIKEY"
+Relay2On="http://octopi/plugin/enclosure/setIO?status=true&index_id=2&apikey=YOURAPIKEY"
+Relay2Off="http://octopi/plugin/enclosure/setIO?status=false&index_id=2&apikey=YOURAPIKEY"
 
 StatusCode=$(curl --silent "http://octopi/plugin/enclosure/getOutputStatus?index_id=3&apikey=YOURAPIKEY")
 
